d000aa6a22fc3b712e6dff43a8c18902db517bd9
Rafael Koji Sales <rafaelkoji7@gmail.com>
2019-04-22 15:15:21 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "../node_modules/react";
+import "./App.css";
+
+import Main from "./pages/Main";
+
+class App extends Component {
+  render() {
+    return <Main />;
+  }
+}
+
+export default App;
